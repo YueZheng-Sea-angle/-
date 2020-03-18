317df7f3b6e933b981921e7990674837baa13b03
unknown <2443029279@qq.com>
2020-03-19 02:45:06 +0800

    gzg

---
+++ 111.py
@@ -0,0 +1,7 @@
+Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 23:03:10) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import time
+print("请猜测歌名“；“咿呀哈哈哈哈哈哈哈”)
+print(".",end="",flush=True)
+for i in range(20):
+time.sleep(0.5)
